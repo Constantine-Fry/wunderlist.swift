2cff43cdc60b29c9b631e20111cc66fef648a96a
Constantine Fry <constantine.fry@gmail.com>
2015-01-13 19:32:45 +0100

    Add demo project for OSX

---
+++ Demo/Demo-OSX/Demo-OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo-OSX
+//
+//  Created by Constantine Fry on 13/01/15.
+//  Copyright (c) 2015 Constantine Fry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

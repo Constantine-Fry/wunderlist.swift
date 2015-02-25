419d6541081c0292bc3d4248915d792ffd693648
Constantine Fry <constantine.fry@gmail.com>
2015-02-25 07:01:52 +0100

    Add methods to lists endopoint

--- Source/Shared/Endpoints/Lists.swift
+++ Source/Shared/Endpoints/Lists.swift
@@ -17,20 +17,79 @@ public class Lists: Endpoint {
         return "lists"
     }
     
-    public func get(completionHandler: (lists: [List]?, error: NSError?) -> Void) -> SessionTask {
+    public func getLists(completionHandler: (lists: [List]?, error: NSError?) -> Void) -> SessionTask {
         return taskWithPath(nil, parameters: nil, HTTPMethod: "GET", transformClosure: { List(JSON: $0) }) {
             (result, error) -> Void in
             completionHandler(lists: result as [List]?, error: error)
         }
     }
     
-    public func create(title: String, completionHandler: (list: List?, error: NSError?) -> Void) -> SessionTask {
+    public func getList(listId: Int, completionHandler: (list: List?, error: NSError?) -> Void) -> SessionTask {
+        let path = String(listId)
+        return taskWithPath(path, parameters: nil, HTTPMethod: "GET", transformClosure: { List(JSON: $0) }) {
+            (result, error) -> Void in
+            completionHandler(list: result as List?, error: error)
+        }
+    }
+    
+    public func getListTasksCount(listId: Int,
+        completionHandler: (count: TasksCount?, error: NSError?) -> Void) -> SessionTask {
+            let path = "tasks_count"
+            let parameters = ["list_id" : listId]
+            return taskWithPath(path, parameters: parameters, HTTPMethod: "GET",
+                transformClosure: { TasksCount(JSON: $0) }) {
+                    (result, error) -> Void in
+                    completionHandler(count: result as TasksCount?, error: error)
+            }
+    }
+    
+    public func createList(title: String, completionHandler: (list: List?, error: NSError?) -> Void) -> SessionTask {
         let parameters = ["title" : title]
         return taskWithPath(nil, parameters: parameters, HTTPMethod: "POST", transformClosure: { List(JSON: $0) }) {
             (result, error) -> Void in
             completionHandler(list: result as List?, error: error)
         }
     }
-
+    
+    public func updateList(listId: Int, title: String, revision: Int,
+        completionHandler: (list: List?, error: NSError?) -> Void) -> SessionTask {
+            let path = String(listId)
+            let parameters = [
+                "revision"  : revision,
+                "title"     : title
+                ] as [String: AnyObject]
+            return taskWithPath(path, parameters: parameters, HTTPMethod: "PATCH",
+                transformClosure: { List(JSON: $0) }) {
+                    (result, error) -> Void in
+                    completionHandler(list: result as List?, error: error)
+            }
+    }
+    
+    public func updateList(listId: Int, isPublic: Bool, revision: Int,
+        completionHandler: (list: List?, error: NSError?) -> Void) -> SessionTask {
+            let path = String(listId)
+            let parameters = [
+                "revision"  : revision,
+                "public"    : isPublic
+                ] as [String: AnyObject]
+            return taskWithPath(path, parameters: parameters, HTTPMethod: "PATCH",
+                transformClosure: { List(JSON: $0) }) {
+                    (result, error) -> Void in
+                    completionHandler(list: result as List?, error: error)
+            }
+    }
+    
+    public func deleteList(listId: Int, revision: Int,
+        completionHandler: (result: Bool, error: NSError?) -> Void) -> SessionTask {
+            let path = String(listId)
+            let parameters = [
+                "revision"  : revision,
+                ] as [String: AnyObject]
+            return taskWithPath(path, parameters: parameters, HTTPMethod: "PATCH",
+                transformClosure: { List(JSON: $0) }) {
+                    (result, error) -> Void in
+                    completionHandler(result: error == nil, error: error)
+            }
+    }
 }
 

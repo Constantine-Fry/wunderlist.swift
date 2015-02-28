360c4da75ce99be512e4e3e9f3fb8837531937cd
Constantine Fry <constantine.fry@gmail.com>
2015-02-28 14:49:41 +0100

    Add login and list fetch in demo project

--- Demo/Demo-iOS/Demo-iOS/FirstViewController.swift
+++ Demo/Demo-iOS/Demo-iOS/FirstViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import WunderlistTouch
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UITableViewController {
+    
+    @IBOutlet weak var loginButton: UIBarButtonItem!
+    
+    var session: Session!
+    
+    var lists: [List]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        session = Session.sharedSession()
+        if session.isAuthorized() {
+            self.fetchLists()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        session.authorize(self) {
+            (result, error) -> Void in
+            if result {
+                self.fetchLists()
+            }
+        }
     }
-
-
+    
+    func fetchLists() {
+        let listTask = self.session.lists.getLists() {
+            (lists, error) -> Void in
+            self.lists = lists
+            self.tableView.reloadData()
+        }
+        listTask.start()
+    }
+    
+    //MARK: - Table View
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        let list = lists?[indexPath.row]
+        cell.textLabel?.text = list?.title
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if lists != nil {
+            return lists!.count
+        }
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44
+    }
 }
 
